7875f87cb0f43899f1a8e796f791d2ee331d8f59
Andres Velasco <af_velasco@misena.edu.co>
2018-04-11 09:17:41 -0500

    v 1.11
    linea

--- src/tiroalblanco/TiroAlBlanco.java
+++ src/tiroalblanco/TiroAlBlanco.java
@@ -43,6 +43,7 @@ public class TiroAlBlanco {
     
     public void desplegar(){
         for(int i=0; i<10; i++){
+            System.out.println("+-+-+-+-+-+-+-+-+-+-+");
             for(int j=0; j<10; j++){
                 System.out.print(" "+cuadro[i][j]);
             }
